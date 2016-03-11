f1c20a51932ed8f0224e722b9190f0268e60c309
lcl306 <lcl306@sohu.com>
2016-03-11 16:05:48 +0800

    first commit

---
+++ src/register/Register.java
@@ -0,0 +1,15 @@
+package register;
+
+
+public class Register {
+	
+	static String encrySeed = "6g402-39gj-53$9b9sj6";
+	
+	public static void main(String[] args) throws Exception{
+		EncryptUtil eu = new EncryptUtil(encrySeed);
+		String code = eu.encrypt("20200601");
+		System.out.println(code);
+		System.out.println(eu.decrypt(code));
+	}
+
+}
