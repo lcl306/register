46f57cfb390cde44cb749164e946e8a2cefcfd5e
lcl306 <lcl306@sohu.com>
2017-05-12 17:56:47 +0800

    change code

---
+++ src/file/CodeChange.java
@@ -0,0 +1,82 @@
+package file;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class CodeChange {
+	
+	public static String JAN_CODE = "Shift_JIS";
+	public static String CN_CODE = "GBK";
+	public static String STD_CODE = "utf-8";
+	public static String ALL_FILE = ".*";
+	
+	public static void main(String[] args)throws Exception{
+		/*change(new File("C:/daifuku/wms/sqlscript/oracle/3.initdata/INIT_DCBase.sql"),
+				new File("C:/daifuku/wms/sqlscript/oracle/3.initdata/UTF8_INIT_DCBase.sql"));*/
+		changeDir("C:/daifuku/wms/sqlscript/oracle/3.initdata",
+				"C:/daifuku/wms/sqlscript/oracle/3.initdataTemp", ALL_FILE);
+		changeDir("C:/daifuku/wms/sqlscript/oracle/2.table",
+				"C:/daifuku/wms/sqlscript/oracle/2.tableTemp", ALL_FILE);
+	}
+	
+	public static void changeDir(String sourceDir, String targetDir, final String fileType)throws Exception{
+		changeDir(new File(sourceDir), new File(targetDir), fileType, CN_CODE, STD_CODE);
+	}
+	
+	public static void changeDir(File sourceDir, File targetDir, final String fileType, 
+			String srcCode, String targetCode)throws Exception{
+		if(sourceDir.isDirectory() && sourceDir.exists()){
+			if(!targetDir.exists()) targetDir.mkdir();
+			File[] files = sourceDir.listFiles(new FilenameFilter(){
+				@Override
+				public boolean accept(File dir, String name) {
+					if(ALL_FILE.equals(fileType)) return true;
+					return name.endsWith(fileType);
+				}
+			});
+			for(File f : files){
+				if(f.isDirectory()){
+					String tfile = targetDir.getPath()+f.getPath().substring(sourceDir.getPath().length());
+					System.out.println(tfile);
+					changeDir(f, new File(tfile), fileType, srcCode, targetCode);
+				}else{
+					change(f, srcCode, new File(targetDir.getPath()+File.separator+f.getName()), targetCode);
+				}
+			}
+		}
+	}
+	
+	public static void change(File source, File target)throws Exception{
+		change(source, JAN_CODE, target, STD_CODE);
+	}
+	
+	public static void change(File source, String srcCode, File target, String targetCode)
+	throws Exception{
+		InputStream in = new FileInputStream(source);
+		InputStreamReader ir = new InputStreamReader(in, srcCode);
+		BufferedReader br = new BufferedReader(ir);
+		OutputStream out = new FileOutputStream(target);
+		OutputStreamWriter iw = new OutputStreamWriter(out, targetCode); 
+		BufferedWriter bw = new BufferedWriter(iw);
+		String line = "";
+		while((line=br.readLine())!=null){
+			bw.write(line);
+			bw.write("\r\n");
+		}
+		bw.close();
+		iw.close();
+		out.close();
+		br.close();
+		ir.close();
+		in.close();
+	}
+
+}
---
+++ src/file/Finder.java
@@ -0,0 +1,60 @@
+package file;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
+
+public class Finder {
+	
+	
+	public static void main(String[] args)throws Exception{
+		//find(new File("C:/daifuku/wms/sqlscript/oracle"), "utf-8", "D:", null);
+		find(new File("C:/daifuku"), "utf-8", "11.2.0", null);
+	}
+	
+	public static final int ENTER_LEN = 2;
+	
+	public static void find(File dir, String srcCode, String targetStr, String replaceStr)throws Exception{
+		if(dir.isDirectory() && dir.exists()){
+			File[] files = dir.listFiles();
+			for(File f : files){
+				if(f.isDirectory()){
+					find(f, srcCode, targetStr, replaceStr);
+				}else{
+					InputStream in = new FileInputStream(f);
+					InputStreamReader ir = new InputStreamReader(in, srcCode);
+					BufferedReader br = new BufferedReader(ir);
+					RandomAccessFile raf = new RandomAccessFile(f,"rw");
+					String line = "";
+					int count = 0;
+					long pointer = 0;
+					while((line=br.readLine())!=null){
+						int pos = line.indexOf(targetStr);
+						if(pos!=-1){
+							count++;
+							if(replaceStr!=null){
+								line = line.replaceAll(targetStr, replaceStr);
+				                raf.seek(pointer);
+				                raf.writeBytes(line);
+							}
+						}
+						pointer+=(line.length()+ENTER_LEN); //pointer总是行首，加2表示上一行的回车和换行符
+                        raf.seek(pointer);//定位位置
+					}
+					if(count>0){
+						System.out.println(f.getPath()+File.separator+f.getName()+": Number of "+targetStr+"="+count);
+					}
+					br.close();
+					ir.close();
+					in.close();
+					if(raf!=null) raf.close();
+				}
+				
+			}
+		}
+	}
+
+}
