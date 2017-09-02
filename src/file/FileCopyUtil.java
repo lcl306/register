43fd6f69285e71a9d935d0bbddd118e1482c5eaa
lcl306 <lcl306@sohu.com>
2017-09-02 23:23:38 +0800

    file copy dir add

--- src/file/FileCopyUtil.java
+++ src/file/FileCopyUtil.java
@@ -4,19 +4,28 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.channels.FileChannel;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class FileCopyUtil {
 	
 	public static void main(String[] args)throws Exception{
 		copy(new File("C:/temp/Module1.bas"), 
-				new File("C:/temp/Module1.bas.bak"));
-		System.out.println(new File("C:/temp/").getPath());
+				new File("C:/temp/sdfsf/gee/se.t"));
+	}
+	
+	public static long copy(File src, File target){
+		long time = 0;
+		if(mkdir(target)){
+			time = _copy(src, target);
+		}
+		return time;
 	}
 	
 	public static final int MAX_BUFF = 20971520;
 	
-	public static long copy(File src, File target){
+	public static long _copy(File src, File target){
         long time=new Date().getTime();
         long length=0;
         try{
@@ -44,5 +53,22 @@ public class FileCopyUtil {
         	return 0l;
         }
     }
+	
+	public static boolean mkdir(File f){
+		List<File> pfs = new ArrayList<>();
+		File pf = f.getParentFile();
+		while(!pf.exists()){
+			pfs.add(pf);
+			pf = pf.getParentFile();
+		}
+		boolean rtn = true;
+		if(!pfs.isEmpty()){
+			for(int i=pfs.size()-1; i>=0; i--){
+				File dir = pfs.get(i);
+				rtn &= dir.mkdir();
+			}
+		}
+		return rtn;
+	}
 
 }
